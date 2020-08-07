b1f092c5b4e05fc86371be9de53858475993a24a
Roberto Torres <roberttv17@gmail.com>
2020-08-07 12:07:24 -0500

    cambios

--- Funciones/Ejercicio_1.py
+++ Funciones/Ejercicio_1.py
@@ -2,7 +2,11 @@
 del rectangulo a partir de una base y una altura. Calcula el área de un rectángulo 
 de 15 de base y 10 de altura: """
 
+area_rect = float(input("Area del rectangulo >> "))
+base_rect = float(input("Base del rectangulo >> "))
+
 def area_rectangulo(base, altura):
     return base * altura
-resultado = area_rectangulo(15, 10)
+
+resultado = area_rectangulo(area_rect, base_rect)
 print(f"El area es {resultado}")
