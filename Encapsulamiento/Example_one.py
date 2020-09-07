548497e38a8194f2abdc77b6509f3746f6cf8ae9
Roberto Torres <roberttv17@gmail.com>
2020-09-07 14:39:18 -0500

    New file

--- Encapsulamiento/Example_one.py
+++ Encapsulamiento/Example_one.py
@@ -3,7 +3,7 @@ class encapsulamiento:
     """ Dos guiones bajos antes del name indican que es privado
     (Solo existe en la clase donde se encuentra) """
 
-    __atri_private = "Atributo privado"
+    __atri_private = 'Atributo privado'
     
     def __function_private(self):
         print("Metodo privado")
@@ -15,4 +15,5 @@ class encapsulamiento:
         return self.__function_private()
 
 ob = encapsulamiento()
-ob.public_atri()
+print(ob.public_atri())
+ob.public_met()
